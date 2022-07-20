7ca1dab0971e2026054c9514a81e6c60c3a461a8
phillipmugisa <phillipmugisa4@gmail.com>
2022-07-20 12:47:53 +0300

    app fully functionally

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k8^b!$oa3hnq4&!the6ufn8rzk0gugk=sv#xy%o0w6$u6jei1_'
+SECRET_KEY = os.environ.get('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
